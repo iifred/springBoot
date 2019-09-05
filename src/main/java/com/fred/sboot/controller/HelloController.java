28b64c4294dd58efb6f76a8e3f843dc87b2531a6
iifred <594703382@qq.com>
2019-09-05 14:44:08 +0800

    111

--- src/main/java/com/fred/sboot/controller/HelloController.java
+++ src/main/java/com/fred/sboot/controller/HelloController.java
@@ -1,4 +1,12 @@
 package com.fred.sboot.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloController {
+    @RequestMapping(value = "hello")
+    public String hello(){
+        return "fred hello!";
+    }
 }
