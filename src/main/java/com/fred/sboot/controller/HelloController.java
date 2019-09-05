425293d9d4750e4e41869e8d38805f71e96904a5
iifred <594703382@qq.com>
2019-09-05 14:44:35 +0800

    Revert "111"
    This reverts commit 28b64c4294dd58efb6f76a8e3f843dc87b2531a6.

--- src/main/java/com/fred/sboot/controller/HelloController.java
+++ src/main/java/com/fred/sboot/controller/HelloController.java
@@ -1,12 +1,4 @@
 package com.fred.sboot.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
 public class HelloController {
-    @RequestMapping(value = "hello")
-    public String hello(){
-        return "fred hello!";
-    }
 }
