b3751afcd840de96fefc0d56d81820ed03aee998
iifred <594703382@qq.com>
2019-09-06 17:44:05 +0800

    thymeleaf试图模板引擎的包导入和初试thy标签

--- src/main/java/com/fred/sboot/controller/HelloController.java
+++ src/main/java/com/fred/sboot/controller/HelloController.java
@@ -4,11 +4,22 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Arrays;
+import java.util.Map;
+
 @Controller
 public class HelloController {
     @RequestMapping(value = "hello")
     @ResponseBody
     public String hello(){
         return "fred";
+    }
+
+    @RequestMapping("success")
+    public String thypageHello(Map<String,Object> map){
+        map.put("hello", "刘志飞");
+        map.put("users", Arrays.asList("fred","rain","jerry"));
+        map.put("msg","<h1>万岁</h1>");
+        return "hello";
     }
 }
