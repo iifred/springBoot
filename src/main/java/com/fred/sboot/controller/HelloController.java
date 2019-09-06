2d38dd903812fe47249710385cd37b151f5e7ca4
iifred <594703382@qq.com>
2019-09-06 14:54:32 +0800

    springbootweb静态资源介绍，默认静态资源文件夹，设置静态资源文件夹，导入webjars的静态包

--- src/main/java/com/fred/sboot/controller/HelloController.java
+++ src/main/java/com/fred/sboot/controller/HelloController.java
@@ -1,4 +1,14 @@
 package com.fred.sboot.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class HelloController {
+    @RequestMapping(value = "hello")
+    @ResponseBody
+    public String hello(){
+        return "fred";
+    }
 }
